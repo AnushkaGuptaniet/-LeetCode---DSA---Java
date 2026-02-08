e890c92f57b2bbee1870f843cb8e93ec0a73fe3f
Anushka Gupta <anushkagupta4503@gmail.com>
2026-02-08 19:51:55 +0530

    Update SortColors.java

--- Arrays/SortColors.java
+++ Arrays/SortColors.java
@@ -53,6 +53,7 @@ public class Colors {
         int low = 0, mid = 0, high = n - 1;
         while (mid <= high) {
             if (nums[mid] == 0) {
+                swap(nums, low, mid);
                 mid++;
                 low++;
             } else if (nums[mid] == 1) {
