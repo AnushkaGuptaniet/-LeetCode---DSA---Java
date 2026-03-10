a2a8cb676bde237777292f91d2636ff163e65534
Anushka Gupta <anushkagupta4503@gmail.com>
2026-03-11 00:38:02 +0530

    BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,3 +1,4 @@
+#Approach 1
 class Solution {
     public int[] bubbleSort(int[] nums) {
         int n = nums.length;
@@ -20,5 +21,36 @@ class Solution {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+}
+//Optimal Approach 
+class Solution {
+
+    public int[] bubbleSort(int[] nums) {
+
+        int n = nums.length;
+
+        for(int i = 0; i < n-1; i++){
+
+            boolean swapped = false;
+
+            for(int j = 0; j < n-i-1; j++){
+
+                if(nums[j] > nums[j+1]){
+
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            if(swapped == false){
+                break;
+            }
+        }
+
+        return nums;
     }
 }
