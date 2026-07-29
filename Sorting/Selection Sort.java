1932038fb23556706145738c89c2a17f7e1b857c
Anushka Gupta <anushkagupta4503@gmail.com>
2026-07-29 13:04:31 +0530

    Selection Sort.java

--- Sorting/Selection Sort.java
+++ Sorting/Selection Sort.java
@@ -1,19 +1,25 @@
+import java.util.Scanner;
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] nums = {7,4,1,5,3};
-        selectionSort(nums);
-        for(int i = 0; i < nums.length; i++){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.print("Enter elements of array: ");
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+        selectionSort(nums, n);
+        for(int i = 0; i < n; i++){
             System.out.print(nums[i] + " ");
         }
-
     }
-    public static void selectionSort(int[] nums){
-        int n = nums.length;
-        for(int i = 0; i < n-1; i++){
+    public static void selectionSort(int[] nums, int n){
+        for(int i = 0; i < n - 1; i++){
             int minIndex = i;
-            for(int j = i+1; j < n; j++){
+            for(int j = i + 1; j < n; j++){
                 if(nums[j] < nums[minIndex]){
-                    minIndex = j ;
+                    minIndex = j;
                 }
             }
             int temp = nums[i];
@@ -22,12 +28,4 @@ public class SelectionSort {
         }
     }
 }
-
-// optimized version (avoid unnecessary swaps) 
-if(minIndex != i){
-int temp = nums[i];
-nums[i]=nums[minIndex];
-nums[minIndex]=temp;
-}
-// TC: O(n^2) for all cases, SC: O(1)
-
+// TC: O(n^2) , SC: O(1)
