3df3126b638f0dcee1bd5ef403537fa756a5cc45
Anushka Gupta <anushkagupta4503@gmail.com>
2026-07-29 14:52:11 +0530

    TwoSum.java

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,30 +1,46 @@
-// PROBLEM 1: TWO SUM
-
-// Approach 1: Brute Force Approach
-import java.util.Arrays;
-public class Solution {
-
+import java.util.*;
+public class TwoSum {
     public static void main(String[] args) {
-        int[] nums = {2,9,11,15};
-        int target = 9;
-        int n = nums.length;
-        System.out.println(Arrays.toString(twoSum(nums, n, target)));
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.print("Enter elements of array: ");
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+        System.out.print("Target : ");
+        int target = sc.nextInt();
+        System.out.println(Arrays.toString(twoSum(nums, target)));
 
-    static int[] twoSum(int[] nums, int n, int target){
-        int sum = 0;
-        for (int i = 0; i < n ; i++){
-            for (int j = i + 1; j < n ; j++){
-                sum = nums[i] + nums[j];
-                if (sum == target){
-                    return new int[]{i, j};
-                }
+    }
+    // brute force
+   public static int[] twoSum(int[] nums, int target){
+       for(int i = 0; i < nums.length; i++){
+           for(int j = i + 1; j < nums.length; j++){
+               if(nums[i] + nums[j] == target){
+                   return new int[]{i, j};
+               }
+           }
+       }
+       return new int[]{};
+   }
+    // TC:O(n^2) , SC: O(1)
+
+    // optimal(hashmap)
+    public static int[] twoSum(int[] nums, int target){
+        HashMap<Integer, Integer>map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
             }
+            map.put(nums[i], i);
         }
         return new int[]{};
     }
+    // tc:O(n), sc :O(n)
 }
-// TC : O(n^2), SC : O(1)
 
 
 // Approach 2 : Two Pointer Approach
@@ -57,28 +73,3 @@ public class Solution {
 // TC : O(n), SC : O(n)
 
 
-// Approach 3: Hashmap (Optimized)
-import java.util.HashMap;
-import java.util.Arrays;
-public class Solution {
-
-    public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
-        int target = 9;
-        int n = nums.length;
-        System.out.println(Arrays.toString(twoSum(nums, n, target)));
-    }
-
-    static int[] twoSum(int[] nums, int n, int target){
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < n; i++){
-            int complement = target - nums[i];
-            if (map.containsKey(complement)){            // checks whether the key is present or not
-                return new int[]{map.get(complement), i};         // return the index of the previous
-            }
-            map.put(nums[i], i);                   // store current no with its index
-        }
-        return new int[]{};
-    }
-}
-// TC : O(n), SC : O(n)
