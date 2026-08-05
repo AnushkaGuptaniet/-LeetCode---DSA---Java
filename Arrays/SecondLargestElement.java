1a0b9442eaaf0ab40d3e1fe95aca459bb5da5a5d
Anushka Gupta <anushkagupta4503@gmail.com>
2026-08-05 11:21:47 +0530

    SecondLargestElement.java

--- Arrays/SecondLargestElement.java
+++ Arrays/SecondLargestElement.java
@@ -1,13 +1,11 @@
-import java.util.*;
-public class SecondLargestElement {
+import java.util.Arrays;
+public class SecondLargest {
     public static void main(String[] args) {
-        int[] nums = {8,8,7,6,5};
-        int n = nums.length;
-        int result = secondLargestElement(nums, n);
-        System.out.print(result);
+        int[] nums = { 8,8,7,6,5};
+        System.out.println(secondLargest(nums));
     }
-    // Approach 1 : brute force (Sorting)
-   public static int secondLargestElement(int[] nums, int n){
+    // brute force
+   public static int secondLargest(int[] nums){
        Arrays.sort(nums);
        int largest = nums[nums.length - 1];
        for(int i = nums.length - 2; i >= 0; i--){
@@ -17,44 +15,42 @@ public class SecondLargestElement {
        }
        return -1;
    }
-    // TC: O(n log n), SC:O(log n)
+    // TC: O(nlogn), SC: O(1)
 
-
-    //Approach 2 : Optimal but 2 traversals
-   public static int secondLargestElement(int[] nums, int n){
+    better
+   public static int secondLargest(int[] nums){
        int largest = nums[0];
-       for(int i = 0; i < n; i++){
+       for(int i = 0; i < nums.length; i++){
            if(nums[i] > largest){
                largest = nums[i];
            }
        }
-       int secondLargest = -1;
-       for(int i = 0; i < n; i++){
-           if(nums[i] != largest && nums[i] > secondLargest){
-               secondLargest = nums[i];
+       int secLargest = -1;
+       for(int i = 0; i < nums.length; i++){
+           if(nums[i] != largest && nums[i] > secLargest){
+               secLargest = nums[i];
            }
        }
-       return secondLargest;
+       return secLargest;
    }
-    // TC: O(n), SC:O(1)
+    // TC: O(n), SC : O(1)
 
-
-    //Approach 3: optimal with one traversal
-    public static int secondLargestElement(int[] nums, int n){
-        int largest = nums[0];
-        int secondLargest = -1;
-        if(n < 2){
-            return -1;
-        }
-        for(int i = 0; i < n; i++){
-            if(nums[i] > largest){
-                secondLargest = largest;
+    // optimal
+    public static int secondLargest(int[] nums) {
+        int largest = Integer.MIN_VALUE;
+        int secLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > largest) {
+                secLargest = largest;
                 largest = nums[i];
-            }else if(nums[i] != largest && nums[i] > secondLargest){
-                secondLargest = nums[i];
+            } else if (nums[i] > secLargest && nums[i] != largest) {
+                secLargest = nums[i];
             }
         }
-        return secondLargest;
+        if(secLargest == Integer.MIN_VALUE){
+            return -1;
+        }
+        return secLargest;
     }
-    // TC:O(n), SC:O(1)
+    // TC: O(n), SC: O(1)
 }
